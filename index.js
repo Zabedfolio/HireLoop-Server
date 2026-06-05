44383315e62b2cb478d5bc9c443c1a41edc2e878
Zabedfolio <zabedfolio@gmail.com>
2026-06-05 16:23:51 +0600

    added basic things

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+
+
 const express = require('express')
 const app = express()
-const port = process.env.PORT;
+require('dotenv').config()
+
+app.use(cors()) // Middleware to enable CORS
+app.use(express.json()) // Middleware to parse JSON bodies
+
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("hireloop-db");
+    const jobCollection = db.collection("jobs");
+
+    app.post('/jobs', async(req,res)=>{
+        const job = req.body;
+        const result = await jobCollection.insertOne(job);
+        res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
